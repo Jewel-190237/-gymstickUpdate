0b0d619e55f1c704e237c5f36023a2717b6d6b3c
Jewel-190237 <jewel190237@gmail.com>
2024-09-18 23:57:01 +0600

    file added

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import Hero from '../../components/home/hero';
-import Features from '../../components/home/feature';
-import AboutUs from '../../components/home/about-us';
-import Services from '../../components/home/services';
-import BMI from '../../components/home/bmi';
-import Testimonial from '../../components/home/testimonial';
-import Team from '../../components/home/team';
-import Shop from '../../components/home/shop';
-import BlogNews from '../../components/home/blog-news';
+// import Hero from '../../components/home/hero';
+// import Features from '../../components/home/feature';
+// import AboutUs from '../../components/home/about-us';
+// import Services from '../../components/home/services';
+// import BMI from '../../components/home/bmi';
+// import Testimonial from '../../components/home/testimonial';
+// import Team from '../../components/home/team';
+// import Shop from '../../components/home/shop';
+// import BlogNews from '../../components/home/blog-news';
 
 const Home = () => {
     return (
         <>
-            <Hero />
+            {/* <Hero />
             <Features/>
             <AboutUs/>
             <Services/>
@@ -20,7 +20,7 @@ const Home = () => {
             <Testimonial/>
             <Team/>      
             <Shop/>  
-            <BlogNews/> 
+            <BlogNews/>  */}
         </>
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import "../styles/common.scss";
-import {GeistSans} from "geist/font/sans";
+import { GeistSans } from "geist/font/sans";
 import Providers from "../providers/userProvider"
+import Navbar from "../components/home/layout/header";
+import Footer from "../components/home/layout/footer";
 export const metadata = {
     title: 'GymStick',
     description: 'Create your next gym experience',
 }
 
-export default function RootLayout({children,}: {children: React.ReactNode}) {
+export default function RootLayout({ children, }: { children: React.ReactNode }) {
     return (
         <html lang="en" className={GeistSans.className}>
-        <Providers>
-            <body>{children}</body>
-        </Providers>
+            <Providers>
+                <body>
+                    <Navbar/>
+                    {children}
+                    <Footer/>
+                </body>
+            </Providers>
         </html>
     )
 }
