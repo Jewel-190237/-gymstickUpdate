4a786c1096139bdce0f95883cf605190358c62c8
Jewel-190237 <jewel190237@gmail.com>
2024-09-24 14:16:11 +0600

    image added

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,13 +4,21 @@ import BasicHeader from '../../components/common/basic-header';
 import FormInput from '../../components/form/input';
 import { Checkbox, Form } from 'antd';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Page: React.FC = () => {
+    const [role, setRole] = useState('member');
+    const [active, setActive] = useState('member');
     const [checked, setChecked] = useState(false);
     const [form] = Form.useForm();
 
-    const onFinish = (values) => {
-        console.log({ ...values, checked });
+    const handleClick = (value: string) => {
+        setRole(value);
+        setActive(value);
+    };
+
+    const onFinish = (values: any) => {
+        console.log({ ...values, checked, role });
         form.resetFields();
     };
 
@@ -18,42 +26,57 @@ const Page: React.FC = () => {
         <div>
             <BasicHeader heading='Log In' subHeading='Home' />
             <div className='px-8 md:px-0 mt-4 max-w-[1320px] mx-auto'>
-                <div className='bg-[#FF4E25] mx-auto max-w-[868px] px-5 md:px-[150px] lg:px-[250px] py-10 md:py-[100px] lg:py-[150px] rounded bg-opacity-10'>
-                    <p className='auth-heading font-montserrat'>Welcome back.</p>
-                    <div className='flex mt-[68px] flex-row items-center space-x-2'>
-                        <div className='h-[2px] bg-[#534C4C] flex-grow'></div>
-                        <p className='description mx-2 text-center'>Sign in with your email</p>
-                        <div className='h-[2px] bg-[#534C4C] flex-grow'></div>
+                <h2 className='login-heading'>Log In Into Your Account </h2>
+                <div className='mt-[116px] flex items-center flex-col md:flex-row space-x-0 md:space-x-14 lg:space-x-20 bg-primary rounded'>
+                    <div className='flex-1 relative'>
+                        <p className='p-0 md:p-6 mt-10 md:mt-0 text-element !text-5xl lg:!text-[130px] md:!text-6xl font-montserrat !text-center'>Gymstick</p>
+                        <Image className='hidden md:flex absolute md:left-9 lg:left-40 -top-[290px]' src='/login.png' alt='Login Image ' width={350} height={780}/>
                     </div>
-                    <div className='mt-4 md:mt-10'>
-                        <Form onFinish={onFinish} form={form}>
-                            <FormInput
-                                label='Email'
-                                placeholder='Email'
-                                name='email'
-                                type='email'
-                                rules={[{ required: true, message: "Please provide your Email" }]}
-                                className="border border-[#D9D9D9] w-full p-4 rounded bg-[#FF4E2508]"
-                            />
-                            <FormInput
-                                label='Password'
-                                placeholder='Password'
-                                name='password'
-                                type='password'
-                                rules={[{ required: true, message: "Please provide your Password" }]}
-                                className="border border-[#D9D9D9] w-full p-4 rounded bg-[#FF4E2508]"
-                            />
-                            <Checkbox className='description mt-6' onChange={e => setChecked(e.target.checked)}>Remember Me?</Checkbox>
-                            <div className='mt-3'><Link className='!text-primary description' href='/forgatpassword'>Forgot Password</Link></div>
-                            <button type='submit' className='auth-button font-poppins py-4'>Continue</button>
-                        </Form>
-                       
-                        <p className='mt-[29px] description !text-center'>New Here? <Link className='text-primary' href='/signup'>Please Sign In</Link></p>
+                    <div className='flex-1 pr-0 md:pr-16 lg:pr-[112px] py-10'>
+                        <div className='text-white'>
+                            <p className='text-[32px] font-bold font-montserrat'>Login as</p>
+                            <div className='mt-10 flex space-x-2 md:space-x-6 lg:space-x-10 font-poppins text-[16px] font-medium'>
+                                <button onClick={() => handleClick('member')} className={`${active === 'member' ? 'underline' : ''}`}>Member</button>
+                                <button onClick={() => handleClick('trainer')} className={`${active === 'trainer' ? 'underline' : ''}`}>Trainer</button>
+                                <button onClick={() => handleClick('admin')} className={`${active === 'admin' ? 'underline' : ''}`}>Admin</button>
+                            </div>
+                            <div className='mt-4 md:mt-10'>
+                                <Form className='space-y-4' onFinish={onFinish} form={form}>
+                                    <Form.Item
+                                        label={<span className="text-white ">Email:</span>}
+                                        rules={[{ required: true, message: "Please provide your Email" }]}
+                                    >
+                                        <FormInput
+                                            name='email'
+                                            placeholder='Email'
+                                            type='email'
+                                            className="border custom-input text-white border-[#D9D9D9] w-full p-4 rounded bg-primary"
+                                        />
+                                    </Form.Item>
+                                    <Form.Item
+                                        label={<span className="text-white">Password:</span>}
+                                        rules={[{ required: true, message: "Please provide your Password" }]}
+                                    >
+                                        <FormInput
+                                            name='password'
+                                            placeholder='Password'
+                                            type='password'
+                                            className="border custom-input text-white border-[#D9D9D9] w-full p-4 rounded bg-primary"
+                                        />
+                                    </Form.Item>
+                                    <div className='flex flex-col md:flex-row justify-between items-center mt-4'>
+                                        <Checkbox className='description !text-white' onChange={e => setChecked(e.target.checked)}>Remember Me?</Checkbox>
+                                        <div className='description !text-white mt-3 md:mt-0'><Link className='!text-white description underline' href='/forgatpassword'>Forgot Password</Link></div>
+                                    </div>
+                                    <button type='submit' className='auth-button !text-primary !bg-white font-poppins py-4 !mt-12'>Log In</button>
+                                </Form>
+                                <p className='mt-4 !text-white description !text-right'>Don’t have an account? <Link className='text-white underline' href='/signup'>Sign up</Link></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     );
 };
-
 export default Page;
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -2,75 +2,98 @@
 import React, { useState } from 'react';
 import BasicHeader from '../../components/common/basic-header';
 import FormInput from '../../components/form/input';
-import { Checkbox, Form, Radio } from 'antd';
+import { Checkbox, Form } from 'antd';
 import Link from 'next/link';
-import { useForm } from 'antd/es/form/Form';
-const Page: React.FC = () => {
-    const [form] = useForm();
-    const [role, setRole] = useState('user');
-    const [checked, setChecked] = useState(false);
+import Image from 'next/image';
 
-    const onFinish = (values) => {
-        console.log({ ...values, role, checked });
+const Page: React.FC = () => {
+    const [role, setRole] = useState('member');
+    const [active, setActive] = useState('member');
+    const [form] = Form.useForm();
+
+    const handleClick = (value: string) => {
+        setRole(value);
+        setActive(value);
+    };
+
+    const onFinish = (values: any) => {
+        console.log({ ...values, role });
         form.resetFields();
     };
+
     return (
         <div>
-            <BasicHeader heading='Create An Account' subHeading='Home' />
-            <div className='px-8 md:px-0 mt-0 md:mt-4 max-w-[1320px] mx-auto'>
-                <div className='bg-[#FF4E25] mx-auto max-w-[868px] px-5 md:px-[150px] lg:px-[250px] py-10 md:py-[100px] lg:py-[150px] rounded bg-opacity-10'>
-                    <h2 className='auth-heading font-montserrat'>let’s join us</h2>
-                    <Radio.Group onChange={e => setRole(e.target.value)} value={role} className='mt-3 md:mt-5 flex justify-center space-x-0 md:space-x-4'>
-                        <Radio className='description font-poppins' value='user'>Sign Up User</Radio>
-                        <Radio className='description font-poppins' value='trainer'>Sign Up Trainer</Radio>
-                    </Radio.Group>
-                    <div className='flex mt-5 md:mt-10 flex-row items-center space-x-2'>
-                        <div className='h-[2px] bg-[#534C4C] flex-grow'></div>
-                        <p className='description mx-2 text-center'>Sign in with your email</p>
-                        <div className='h-[2px] bg-[#534C4C] flex-grow'></div>
+            <BasicHeader heading='Sign Up' subHeading='Home' />
+            <div className='px-8 md:px-0 mt-4 max-w-[1320px] mx-auto'>
+                <h2 className='login-heading'>Create An Account</h2>
+                <div className='mt-[116px] flex items-center flex-col md:flex-row space-x-0 md:space-x-14 lg:space-x-20 bg-primary rounded'>
+                    <div className='flex-1 relative'>
+                        <p className='p-0 md:p-6 mt-10 md:mt-0 text-element !text-5xl lg:!text-[130px] md:!text-6xl font-montserrat !text-center'>Gymstick</p>
+                        <Image className='hidden md:flex absolute md:left-9 lg:left-40 -top-[290px]' src='/login.png' alt='Login Image ' width={350} height={780}/>
                     </div>
-                    <Form className='mt-4 md:mt-[34px]' form={form} onFinish={onFinish}>
-                        <FormInput
-                            label='Name'
-                            placeholder='Name'
-                            name='name'
-                            type='text'
-                            rules={[{ required: true, message: "Please provide your Name" }]}
-                            className="border border-[#D9D9D9] w-full p-[18px] rounded bg-[#FF4E2508]"
-                        />
-                        <FormInput
-                            label='Email'
-                            placeholder='Email'
-                            name='email'
-                            type='email'
-                            rules={[{ required: true, message: "Please provide your Email" }]}
-                            className="border border-[#D9D9D9] w-full p-[18px] rounded bg-[#FF4E2508]"
-                        />
-                        <FormInput
-                            label='Password'
-                            placeholder='Password'
-                            name='password'
-                            type='password'
-                            rules={[{ required: true, message: "Please provide your Password" }]}
-                            className="border border-[#D9D9D9] w-full p-4 rounded bg-[#FF4E2508]"
-                        />
-                        <FormInput
-                            label='Re-Type Password'
-                            placeholder='Re-Type Password'
-                            name='reTypePassword'
-                            type='password'
-                            rules={[{ required: true, message: "Please provide your Re-Type Password" }]}
-                            className="border border-[#D9D9D9] w-full p-4 rounded bg-[#FF4E2508]"
-                        />
-
-                        <Checkbox className='description mt-[22px]' onChange={e => setChecked(e.target.checked)}>Remember Me?</Checkbox>
-                        <button type='submit' className='auth-button font-poppins mt-[34px] py-4'>Continue</button>
-                    </Form>
-                    <p className='mt-3 md:mt-[29px] description !text-center'>Already have an account? <Link className='text-primary' href='/login'> Log in now</Link></p>
+                    <div className='flex-1 pr-0 md:pr-16 lg:pr-[112px] py-10'>
+                        <div className='text-white'>
+                            <p className='text-[32px] font-bold font-montserrat'>let’s join us</p>
+                            <div className='mt-10 flex space-x-2 md:space-x-6 lg:space-x-10 font-poppins text-[16px] font-medium'>
+                                <button onClick={() => handleClick('member')} className={`${active === 'member' ? 'underline' : ''}`}>Member</button>
+                                <button onClick={() => handleClick('trainer')} className={`${active === 'trainer' ? 'underline' : ''}`}>Trainer</button>
+                            </div>
+                            <div className='mt-4 md:mt-10'>
+                                <Form className='space-y-4' onFinish={onFinish} form={form}>
+                                    <Form.Item
+                                        label={<span className="text-white ">Name:</span>}
+                                        rules={[{ required: true, message: "Please provide your Name" }]}
+                                    >
+                                        <FormInput
+                                            name='name'
+                                            placeholder='Name'
+                                            type='text'
+                                            className="border custom-input text-white border-[#D9D9D9] w-full p-4 rounded bg-primary"
+                                        />
+                                    </Form.Item>
+                                    <Form.Item
+                                        label={<span className="text-white ">Email:</span>}
+                                        rules={[{ required: true, message: "Please provide your Email" }]}
+                                    >
+                                        <FormInput
+                                            name='email'
+                                            placeholder='Email'
+                                            type='email'
+                                            className="border custom-input text-white border-[#D9D9D9] w-full p-4 rounded bg-primary"
+                                        />
+                                    </Form.Item>
+                                    <Form.Item
+                                        label={<span className="text-white">Password:</span>}
+                                        rules={[{ required: true, message: "Please provide your Password" }]}
+                                    >
+                                        <FormInput
+                                            name='password'
+                                            placeholder='Password'
+                                            type='password'
+                                            className="border custom-input text-white border-[#D9D9D9] w-full p-4 rounded bg-primary"
+                                        />
+                                    </Form.Item>
+                                    <Form.Item
+                                        label={<span className="text-white">Re-Type Password:</span>}
+                                        rules={[{ required: true, message: "Please provide your Password again" }]}
+                                    >
+                                        <FormInput
+                                            name='re-password'
+                                            placeholder='Re Password'
+                                            type='password'
+                                            className="border custom-input text-white border-[#D9D9D9] w-full p-4 rounded bg-primary"
+                                        />
+                                    </Form.Item>
+                                    
+                                    <button type='submit' className='auth-button !text-primary !bg-white font-poppins py-4 !mt-12'>Log In</button>
+                                </Form>
+                                <p className='mt-4 !text-white description !text-left'>Already have an account <Link className='text-white underline' href='/login'>Log In</Link></p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     );
 };
-
 export default Page;
